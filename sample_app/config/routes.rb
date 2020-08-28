fc08c806588220a9d0a5682d5ebfffa8dc792f38
Shingo-Kuroyanagi <ec2-user@ip-172-31-13-74.ec2.internal>
2020-08-28 09:40:59 +0000

    [Start]2-8

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
  get 'todolists/:id' => 'todolists#show', as: 'todolist'
  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
  patch 'todolists/:id'=>'todolists#update', as: 'update_todolist'
-end
+ delete 'todolists/:id' => 'todolist#destory', as: 'destory_todolist'
+ end
