84560f691af06b1cdfe1231fd031558e6d82bcde
Shingo-Kuroyanagi <ec2-user@ip-172-31-13-74.ec2.internal>
2020-08-28 09:11:08 +0000

    [Finish]2-7

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9a972a9ba16b17773476e93895e6c7024a1012b1fd78932bcb7b03c8a70cba46fbd0b8af568bfb648ef95302cb26e943b6837d7bf07d276ad8352f57fed77fa8'
