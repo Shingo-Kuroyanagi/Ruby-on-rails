3b24992005ad0387e4de801cde33ca3d655fcf21
Shingo-Kuroyanagi <ec2-user@ip-172-31-13-74.ec2.internal>
2020-09-04 14:43:18 +0000

    [Finish]3-3

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -7,12 +7,13 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
   end
   
   def create
-   blog= Blog.new(blog_params)
-   blog.save
-   redirect_to blogs_path
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
   end
 
   def edit
@@ -20,6 +21,6 @@ class BlogsController < ApplicationController
   
   private
   def blog_params
-    params.require(:blog) .permit(:title,:category,:body)
+    params.require(:blog).permit(:title, :category, :body)
   end
 end
