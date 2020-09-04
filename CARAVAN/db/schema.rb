1c98ceca79e25191bc27f4a40a5abe26aee0e1a7
Shingo-Kuroyanagi <ec2-user@ip-172-31-13-74.ec2.internal>
2020-09-04 16:05:46 +0000

    [Start]3-4

--- CARAVAN/db/schema.rb
+++ CARAVAN/db/schema.rb
@@ -10,9 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_075945) do
+ActiveRecord::Schema.define(version: 2020_09_04_154104) do
 
   create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.string "category"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
